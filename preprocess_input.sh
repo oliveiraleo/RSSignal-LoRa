fb93d66e5dce0837fa2b44a57c143b3a3ee4ad91
Leo <leoferenzini@gmail.com>
2022-04-01 19:05:28 -0300

    Adds more functionality to script params

--- preprocess_input.sh
+++ preprocess_input.sh
@@ -1,35 +1,113 @@
 #!/bin/sh
-# This is bash program to filter the data
-# and remove any char that is not a number
-# but keeping the negative numbers intact
+: ' This is bash program to filter the data
+   and remove any char that is not a number
+   but keeping the negative numbers intact
+  '
 
 path=$(pwd) # var to store the uptaded path
 
 # Help function, displays the usage of the script
 Help()
 {
+  echo #new line
   echo "Usage:"
   echo $0 "input_file_name [options]"
+  echo #new line
   echo "Options:"
-  echo "-t  --test     Enables the test mode (only displays the result on stdout)"
-  echo "-o  --output   Specifies the output file where results will be saved"
+  echo "-t  --test        Enables the test mode (only displays the result on stdout)"
+  echo "-o  --output      Specifies the output file where results will be saved (WARNING: it will overwrite the file specified by this param)"
+  echo "-c  --column      Specifies which column to get data from (default is 0, which prints all columns)"
+  echo "-s  --separator   Specifies which separator to use when reading data (defaults to SPACE)"
+  echo #new line
   echo "Examples:"
   echo $0 "input.txt -t"
+  echo $0 "input.txt -t -c 2"
+  echo $0 "input.txt -t -s '\n'"
+  echo $0 "input.txt -t -c 2 -s '\n'"
   echo $0 "input.txt -o output.txt"
+  echo $0 "input.txt -o output.txt -c 1"
+  echo $0 "input.txt -o output.txt -s '\t'"
+  echo $0 "input.txt -o output.txt -c 1 -s '\t'"
   exit
 }
 
-# Main functionality
-case "$2" in
-  -o|--output)
-    awk '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print}' $1 >> $3 #filters only negative numbers with 2 or 3 digits
-    echo "The preprocessed results were saved at" $path"/"$3
-    ;;
-  -t|--test)
-    echo "WARNING: Test mode is enabled"
-    awk '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print}' $1 #filters only negative numbers with 2 or 3 digits
-    echo "CAUTION: This is only a preview, the results were NOT saved yet"
-    ;;
-  *)
+# prints an error message to stderr
+err()
+{
+    echo "E: $*" >>/dev/stderr
+}
+
+PrintSuccessfulSave()
+{
+  echo "The preprocessed results were saved at" $path"/"$1
+}
+
+OptionColumnAndSeparatorWithSave()
+{
+  #echo $@ $#
+  if [[ $4 == "-c" ]] && [[ $6 == "-s" ]] && [ $# -eq 7 ]; then #checks if the parameters are correct
+    awk -F $7 '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print $'$5'}' $1 > $3
+    PrintSuccessfulSave $3
+  elif [[ $4 == "-c" ]] && [ $# -eq 5 ]; then #checks if the parameters are correct
+    awk '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print $'$5'}' $1 > $3
+    PrintSuccessfulSave $3
+  elif [[ $4 == "-s" ]] && [ $# -eq 5 ]; then #checks if the parameters are correct
+    awk -F $5 '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print}' $1 > $3
+    PrintSuccessfulSave $3
+  else
+    err "Error processing options... Please check them and try again"
     Help
-esac
+  fi
+}
+
+OptionColumnAndSeparator()
+{
+  #echo $@ $#
+  if [[ $3 == "-c" ]] && [[ $5 == "-s" ]] && [ $# -eq 6 ]; then #checks if the parameters are correct
+    awk -F $6 '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print $'$4'}' $1
+  elif [[ $3 == "-c" ]] && [ $# -eq 4 ]; then #checks if the parameters are correct
+    awk '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print $'$4'}' $1
+  elif [[ $3 == "-s" ]] && [ $# -eq 4 ]; then #checks if the parameters are correct
+    awk -F $4 '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print}' $1
+  else
+    err "Error processing options... Please check them and try again"
+    Help
+  fi
+}
+
+# Main functionality
+Main()
+{
+  #checks if the number of ARGS are ok
+  if [ $# -lt 2 ]; then
+    err "Not enough ARGS!"
+  elif [ $# -gt 7 ]; then
+    err "Too many ARGS!"
+    
+    Help
+  fi
+  #tests which execution flow to execute
+  case "$2" in
+    -o|--output)
+      if [ $# -eq 3 ]; then
+        awk '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print}' $1 > $3 #filters only negative numbers with 2 or 3 digits
+        PrintSuccessfulSave $3
+      else
+        OptionColumnAndSeparatorWithSave $@ #$@ sends all the ARGS from main to the function scope
+      fi
+      ;;
+    -t|--test)
+      echo "WARNING: Test mode is enabled"
+      if [ $# -eq 2 ]; then
+        awk '/-[0-9][0-9]$|-[0-9][0-9][0-9]$/{print}' $1 #filters only negative numbers with 2 or 3 digits
+      else
+        OptionColumnAndSeparator $@ #$@ sends all the ARGS from main to the function scope
+      fi
+      echo "CAUTION: This is only a preview, the results were NOT saved yet"
+      ;;
+    *)
+      Help
+  esac
+}
+
+Main $@ #calls the program's main
